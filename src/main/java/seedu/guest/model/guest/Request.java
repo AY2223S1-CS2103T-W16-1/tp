57fa6249493a2dc1d5b7c3fdb13b722304cf1d32
Lan-Jingbo <lanjingbo114@163.com>
2022-10-17 22:58:15 +0800

    Resolve messy code

--- src/main/java/seedu/guest/model/guest/Request.java
+++ src/main/java/seedu/guest/model/guest/Request.java
@@ -10,17 +10,10 @@ public class Request {
     public final String requests;
 
     /**
-<<<<<<< Updated upstream
      * There are no request.
      */
     public Request() {
-        this.requests = "";
-=======
-     * When there are no request
-     */
-    public Request() {
         requests = "";
->>>>>>> Stashed changes
     }
 
     /**
