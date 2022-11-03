0a56440fd06f54ded4f49badf3b1a50fbd6aaaf7
Lan-Jingbo <lanjingbo114@163.com>
2022-11-03 14:40:28 +0800

    JavaDoc

--- src/main/java/seedu/guest/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/guest/logic/commands/RemarkCommand.java
@@ -1,6 +1,13 @@
 package seedu.guest.logic.commands;
 
+/**
+ * A RemarkCommand Class for tutorial requirement.
+ */
 public class RemarkCommand {
+
+    /**
+     * The constructor for RemarkCommand.
+     */
     public RemarkCommand() {
         System.out.println("for tutorial");
     }
